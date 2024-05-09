ab21145e52b2448869f1da38853c6e289ffc0d68
Александр Николаев <alexandrnikolaev1@gmail.com>
2024-05-09 12:33:20 +0300

    Добавьте файлы проекта.

---
+++ ToDo.Domain/Entity/TaskEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Domain.Entity
+{
+    internal class TaskEntity
+    {
+        public long id { get; set;}
+        
+        public string Name { get; set;}
+       
+        public string Description { get; set;}
+
+        public Enum.Priority Priority { get; set;}
+
+
+    }
+}
---
+++ Тудушка/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Тудушка.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
